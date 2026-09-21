d17c429e5be15da7684259f1538881e0581bc507
viveksinghthikhai0824-coder <viveksinghthikhai0824@gmail.com>
2026-09-21 11:50:51 +0530

    index.js added

---
+++ index.js
@@ -0,0 +1,11 @@
+let employee = {
+    name: "vivek",
+    age:25,
+    gender:"male",
+    blood_group:"O+",
+    salary: {
+        bank_account:47071051001544
+    },
+    skills:["python,CSS,java"],
+}
+console.log(employee);
